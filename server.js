da166275b610df328140e5f4eaba16ce32107826
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-03-25 20:07:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Dalai = require('dalai');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
-const dalai = new Dalai("/home/dan/dalai/")
+const dalai = new Dalai("<path/to/dalai/>")
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -13,37 +13,38 @@ const server = http.createServer(app);
 
 app.post('/dalai_request', (req, res) => {
   
-    const input = req.body.input;
-    const chunks = [];
+  const input = req.body.input;
+  const chunks = [];
 
-    
-    console.log(input);
-    dalai.request({
-      model: "alpaca.7B",
-      prompt: input,
-    }, (token) => {
-      console.log(token);
-      chunks.push(token);
-    })
-    .then(() => {
-      const response = chunks.join('');
-      const startIndex = response.indexOf('\r\n');
-      let truncatedResponse = '';
-  
-      if (startIndex !== -1) {
-        truncatedResponse = response.slice(startIndex + 2); 
-      } else {
-        truncatedResponse = response;
-      }
-  
-      res.json({ chatsays: truncatedResponse });
-    })
+  console.log(input);
+  dalai.request({
+    model: "alpaca.7B",
+    prompt: input,
+  }, (token) => {
+    console.log(token);
+    chunks.push(token);
+  })
+  .then(() => {
+    const response = chunks.join('');
+    const responseMarker = '### Response:';
+    const startIndex = response.indexOf(responseMarker);
+    let truncatedResponse = '';
+
+    if (startIndex !== -1) {
+      truncatedResponse = response.slice(startIndex + responseMarker.length).trim();
+    } else {
+      truncatedResponse = response;
+    }
+
+    res.json({ chatsays: truncatedResponse });
+  })
 });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
